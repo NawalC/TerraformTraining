5f45b9d3d7fc0156c92bacaead60af469841f14d
Nawal C <nawal.abdul-careem@capgemini.com>
2021-08-05 22:31:34 +0100

    added public ip

---
+++ v15/providers.tf
@@ -0,0 +1,15 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.15.5"
+}
+
+provider "azurerm" {
+  features {}
+}
