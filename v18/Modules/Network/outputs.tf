4d820e0c62191cc4bdb99cd018b2933d5eda07d7
Nawal C <nawal.abdul-careem@capgemini.com>
2021-08-10 21:37:23 +0100

    moved load balancer and public ip address to a module

---
+++ v18/Modules/Network/outputs.tf
@@ -0,0 +1,9 @@
+# Outputs for use by other modules
+output "subnet_id" {
+  value = azurerm_subnet.vm_subnet.id
+}
+
+output "network_id" {
+  value = azurerm_virtual_network.vm_network.id
+}
+
---
+++ v18/Modules/public_load_balancer/main.tf
@@ -0,0 +1,4 @@
+# Configure the Azure provider
+terraform {
+  required_version = "~> 0.15.5"
+}
